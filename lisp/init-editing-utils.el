6ce26cad3110744116e68dd76b43fddb69adda9d
dejavu <dejavu@dejavu.local>
2018-02-27 23:21:05 +0800

    fix rainbow bugs

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -1,9 +1,13 @@
 (when (require-package 'rainbow-delimiters)
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  ;; rainbow-delimiters-mode not work if outline-minor-mode not set
+  (outline-minor-mode +1)
+  (outline-minor-mode -1))
 
 (show-paren-mode +1)
 (when (require-package 'smartparens)
-  (add-hook 'prog-mode-hook 'smartparens-mode))
+  (add-hook 'prog-mode-hook 'smartparens-mode)
+  (require 'smartparens-config))
 
 (require-package 'clang-format)
 
--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,4 +1,3 @@
-(add-hook 'emacs-lisp-mode-hook (rainbow-delimiters-mode +1))
 (add-hook 'emacs-lisp-mode-hook (smartparens-mode +1))
 
 (provide 'init-lisp)
