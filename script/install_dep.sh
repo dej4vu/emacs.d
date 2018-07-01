727650c15f05a0097ecece15861567a576df8c51
dejavu <dejavu@dejavu.local>
2018-07-01 17:02:40 +0800

    add go dep install script

---
+++ script/install_dep.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+for dep in "
+	github.com/nsf/gocode
+	github.com/josharian/impl
+	golang.org/x/tools/cmd/godoc
+	github.com/davidrjenni/reftools/cmd/fillstruct
+	github.com/fatih/gomodifytags
+	golang.org/x/tools/cmd/guru
+	github.com/rogpeppe/godef
+	golang.org/x/lint/golint
+	golang.org/x/tools/cmd/gorename
+	github.com/sqs/goreturns
+"
+do
+	go get -v -u $dep
+done
