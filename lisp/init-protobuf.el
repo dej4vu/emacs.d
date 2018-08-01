83290f72a7a42727a4d07093025bbeb081d47180
chenchang <origineme@gmail.com>
2018-08-01 10:10:44 +0800

    update protobuf indent

--- lisp/init-protobuf.el
+++ lisp/init-protobuf.el
@@ -1,12 +1,19 @@
 (require-package 'protobuf-mode)
 
+;; use 4-space indentation:
+(defconst my-protobuf-style
+  '((c-basic-offset . 4)
+    (indent-tabs-mode . nil)))
+
 (eval-after-load 'protobuf-mode
   '(progn
      ;; format on save
-     (add-hook 'before-save-hook 'clang-format-buffer)
+     ;;(add-hook 'before-save-hook 'clang-format-buffer)
 
      ;; flycheck mode
      (add-hook 'protobuf-mode-hook 'flycheck-mode)
+	 (add-hook 'protobuf-mode-hook
+			   (lambda () (c-add-style "my-style" my-protobuf-style t)))
      )
   )
 
