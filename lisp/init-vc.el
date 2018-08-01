843f2508cfbbc6baef4ede068f4aa18cb4b95f7a
chenchang <origineme@gmail.com>
2018-08-01 10:11:15 +0800

    add vs diff hl

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,5 +1,7 @@
 (require-package 'magit)
 (require-package 'diff-hl)
 
+(global-diff-hl-mode +1)
+(diff-hl-margin-mode +1)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 (provide 'init-vc)
