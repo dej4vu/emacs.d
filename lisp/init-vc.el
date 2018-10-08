8a5683f0e8bf81c32f90df1d18d9ac18932d2ad3
chenchang <origineme@gmail.com>
2018-10-08 17:01:59 +0800

    update vc and ui

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -3,5 +3,6 @@
 
 (global-diff-hl-mode +1)
 (diff-hl-margin-mode +1)
+(global-set-key (kbd "C-x g") 'magit-status)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 (provide 'init-vc)
