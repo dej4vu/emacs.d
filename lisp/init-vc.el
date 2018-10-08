7a1b7fca94ec827f65fabe6b56dc720010f99798
dej4vu <dejavu@dejavu.local>
2018-10-09 00:40:48 +0800

    enable diff-hl-flydiff-mode

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -3,6 +3,7 @@
 
 (global-diff-hl-mode +1)
 (diff-hl-margin-mode +1)
+(diff-hl-flydiff-mode +1)
 (global-set-key (kbd "C-x g") 'magit-status)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 (provide 'init-vc)
