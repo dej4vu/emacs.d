76957712988ba8110d5e408f8acaf7bf5a55b41e
dej4vu <dejavu@dejavu.local>
2019-06-14 17:46:35 +0800

    update flycheck settings

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,4 +1,7 @@
 (require-package 'flycheck)
 (setq flycheck-error-list-minimum-level 'error)
+(setq flycheck-idle-change-delay 3)
+(setq flycheck-idle-buffer-switch-delay 5)
+(setq flycheck-check-syntax-automatically '(idle-change save))
 
 (provide 'init-flycheck)
