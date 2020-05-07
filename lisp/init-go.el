f38d15fb7a2ddc28407962a2fc17310491b0b916
dej4vu <origineme@gmail.com>
2020-05-07 17:54:48 +0800

    update go mode

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,12 +1,15 @@
 (require-package 'go-mode)
 (require-package 'go-eldoc)
-(require-package 'company-go)
+;;(require-package 'company-go)
 ;; (require-package 'go-add-tags)
 (require-package 'go-guru)
 (require-package 'go-tag)
 (require-package 'go-fill-struct)
 (require-package 'go-impl)
 (require-package 'go-rename)
+(require-package 'lsp-mode)
+(require-package 'lsp-ui)
+(require-package 'company-lsp)
 
 (add-auto-mode 'go-mode
 	       "\\.go\\'")
@@ -17,14 +20,17 @@
      ;; gofmt on save
      (add-hook 'before-save-hook 'gofmt-before-save)
 
+	 (add-hook 'go-mode-hook 'lsp-deferred)
+
      ;; company mode settings
-     (add-to-list 'company-backends 'company-go)
+     ;;(add-to-list 'company-backends 'company-go)
 
      ;; el-doc for go
      (go-eldoc-setup)
 
      ;; flycheck mode
      ;;(add-hook 'go-mode-hook 'flycheck-mode)
+	 (push 'company-lsp company-backends)
 	 (add-hook 'go-mode-hook (lambda ()
                                   (flycheck-mode 1);;))
                                   (setq flycheck-checker 'go-golint)))
