193fb75b9176aa6c8c71a36954b9d86dd8c4f1d1
dej4vu <origineme@gmail.com>
2021-09-29 17:19:28 +0800

    update go-mode

--- script/install_dep.sh
+++ script/install_dep.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
-mkdir tmp
-export GOPATH=$(pwd)/tmp
-export GOPROXY=https://goproxy.io
+#mkdir tmp
+#export GOPATH=$(pwd)/tmp
+export GOPROXY=https://goproxy.cn,https://goproxy.io,direct
 export GO111MODULE=on
-for dep in "
-	github.com/stamblerre/gocode
-	github.com/josharian/impl
-	golang.org/x/tools/cmd/godoc
-	github.com/davidrjenni/reftools/cmd/fillstruct
-	github.com/fatih/gomodifytags
-	golang.org/x/tools/cmd/guru
-	github.com/rogpeppe/godef
-	golang.org/x/lint/golint
-	golang.org/x/tools/cmd/gorename
-	github.com/sqs/goreturns
-	golang.org/x/tools/gopls@latest
+#go clean -modcache
+deps="github.com/stamblerre/gocode@latest \
+	github.com/josharian/impl@latest \
+	golang.org/x/tools/cmd/godoc@latest\
+	github.com/davidrjenni/reftools/cmd/fillstruct@latest\
+	github.com/fatih/gomodifytags@latest\
+	golang.org/x/tools/cmd/guru@latest\
+	github.com/rogpeppe/godef@latest\
+	golang.org/x/lint/golint@latest\
+	golang.org/x/tools/cmd/gorename@latest\
+	github.com/sqs/goreturns@latest\
+	golang.org/x/tools/gopls@latest\
 "
+for dep in $deps
 do
-	go get -v -u $dep
+	echo "installing $dep"
+	go install -v  $dep
 done
-go clean -modcache
