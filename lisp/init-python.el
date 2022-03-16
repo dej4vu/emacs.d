92785b7da037677d8bd766a9838627bac6fcfc07
dej4vu <origineme@gmail.com>
2022-03-16 19:37:56 +0800

    add  python

---
+++ lisp/init-python.el
@@ -0,0 +1,6 @@
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+(provide 'init-python)
