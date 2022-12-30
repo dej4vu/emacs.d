b2a8182f4db3150f4f1541b49bf906cb72705090
dej4vu <origineme@gmail.com>
2022-12-30 15:57:53 +0800

    update exec-path

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,8 +1,11 @@
-(when (and window-system
-		   (memq window-system '(mac ns)))
-  (use-package exec-path-from-shell
-	:init
-	(exec-path-from-shell-initialize)))
-
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns x))
+  :ensure t
+  :init
+  (exec-path-from-shell-copy-env "GOPATH")
+  (exec-path-from-shell-copy-env "GOPROXY")
+  :config
+  (exec-path-from-shell-initialize)
+  (message "Wellcome To Mac OS X, Have A Nice Day!!!"))
 
 (provide 'init-exec-path)
