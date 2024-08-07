fd0f4f228deb3627d81b09a3a6191153a3c61be4
dej4vu <origineme@gmail.com>
2024-08-07 16:24:53 +0800

    update install_dep.sh

--- script/install_dep.sh
+++ script/install_dep.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
-mkdir tmp
-export GOPATH=$(pwd)/tmp
+tmp=$(mktemp -d -t go-build)
+dst="${1:-$HOME/bin}"
+
+#go env setup
+export GOPATH=$tmp
 export GOPROXY=https://goproxy.cn,https://goproxy.io,direct
 export GO111MODULE=on
-#go clean -modcache
-deps="github.com/stamblerre/gocode@latest \
-	github.com/josharian/impl@latest \
-	golang.org/x/tools/cmd/godoc@latest\
+echo "GOPATH: $GOPATH"
+deps="github.com/josharian/impl@latest \
 	github.com/davidrjenni/reftools/cmd/fillstruct@latest\
 	github.com/fatih/gomodifytags@latest\
-	golang.org/x/tools/cmd/guru@latest\
-	github.com/rogpeppe/godef@latest\
 	golang.org/x/lint/golint@latest\
-	golang.org/x/tools/cmd/gorename@latest\
-	github.com/sqs/goreturns@latest\
 	golang.org/x/tools/gopls@latest\
 "
 for dep in $deps
@@ -21,3 +18,11 @@ do
 	echo "installing $dep"
 	go install -v  $dep
 done
+
+echo "moving binary files to $dst"
+mv $tmp/bin/* $dst
+
+#cleanup tmp files
+echo "clean up tmp files"
+go clean -cache -modcache
+rm -rf $tmp
